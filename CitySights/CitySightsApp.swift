e30aab4a05818f67a7064b157cd0a0b8da4dadd8
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-08-24 00:50:16 +0600

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Shrikanta Mazumder on 24/8/21.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
